0e8c543416937cdffef0f083e91d1d7692a46c63
amanda-bann <amanda.banner@gmail.com>
2026-06-19 14:42:28 -0600

    View a full recipe in the ui

--- src/App.js
+++ src/App.js
@@ -1,35 +1,49 @@
 import React, {useState, useEffect} from "react";
 import Header from "./components/Header";
 import RecipeExcerpt from "./components/RecipeExcerpt";
+import RecipeFull from "./components/RecipeFull";
 import "./App.css";
 
 function App() {
   const [recipes, setRecipes] = useState([]);
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
 
-useEffect(() => {
-  const fetchAllRecipes = async () => {
-    try {
-      const response = await fetch("/api/recipes");
-      if (response.ok){
-        const data = await response.json();
-        setRecipes(data);
-      } else {
-        console.log("Whoops! Could not fetch recipes!")
+  useEffect(() => {
+    const fetchAllRecipes = async () => {
+      try {
+        const response = await fetch("/api/recipes");
+        if (response.ok){
+          const data = await response.json();
+          setRecipes(data);
+        } else {
+          console.log("Whoops! Could not fetch recipes!")
+        }
+      } catch (e) {
+        console.error("An error occured during the request", e)
       }
-    } catch (e) {
-      console.error("An error occured during the request", e)
-    }
+    };
+    fetchAllRecipes();
+  }, []);
+
+  const handleSelectRecipe =  (recipe) => {
+    setSelectedRecipe(recipe);
   };
-  fetchAllRecipes();
-}, []);
+
+  const handleUnselectRecipe = () => {
+    setSelectedRecipe(null);
+  };
 
 
   return (
     <div className='recipe-app'>
       <Header />
-      {recipes.map((recipe) => (
-        <RecipeExcerpt key={recipe.id} recipe={recipe} />
-      ))}
+      {selectedRecipe ? <RecipeFull selectedRecipe={selectedRecipe} handleUnselectRecipe={handleUnselectRecipe} />
+        : <div className='recipe-list'>
+          {recipes.map((recipe) => (
+            <RecipeExcerpt key={recipe.id} recipe={recipe} handleSelectRecipe={handleSelectRecipe}/>
+          ))}
+        </div>
+      }
     </div>
   );
 }

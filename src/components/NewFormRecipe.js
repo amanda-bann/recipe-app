3673ce214e2f97edd2675ddc60c7a3d1f5201f80
amanda-bann <amanda.banner@gmail.com>
2026-07-12 17:40:12 -0600

    Create recipe form for front end

---
+++ src/components/NewFormRecipe.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const NewFormRecipe = () => {
+    return (
+        <div className='recipe-details'>
+                <div className='recipe-form'>
+                    <h2>New Recipe</h2>
+                <button className='cancel-button'>Cancel</button>
+
+                <form>
+                    <label htmlFor="title">Title</label>
+                    <input id="title" type="text" name='title' value='' onChange='' required />
+                    <label htmlFor="ingredients">Ingredients</label>
+                    <textarea
+                        id="ingredients"
+                        name='ingredients'
+                        value=''
+                        onChange=''
+                        required
+                        placeholder='Add ingredients separated by commas - i.e. flour, sugar, almonds'
+                    />
+
+                    <label htmlFor="instructions">Instructions</label>
+                    <textarea id="instruction" name='instructions' value='' onChange='' required />
+
+                    <label htmlFor="description">Description</label>
+                    <textarea id="description" name='description' value='' onChange='' required />
+
+                    <label htmlFor="servings">Servings</label>
+                    <input id="servings" type="number" name='servings' value='' onChange='' required />
+
+                    <button type='submit'>Save Recipe</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default NewFormRecipe;
